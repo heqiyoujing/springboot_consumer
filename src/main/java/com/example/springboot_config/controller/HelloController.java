bb079351bd69afd0a93fae8ff1315e00dbd4874f
heqiyoujing <38516694+heqiyoujing@users.noreply.github.com>
2018-06-07 19:28:15 +0800

    Add files via upload

---
+++ src/main/java/com/example/springboot_config/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.springboot_config.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+
+   /* @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return helloService.helloService();
+    }*/
+}
